49be57d5cd79ce6f6f18cd4a49309e43059b35ad
dtrolley <77252655+dtrolley@users.noreply.github.com>
2025-11-17 15:13:32 -0500

    Revert "fix: prevent unknown action error on settings page"
    This reverts commit afce4b5b6d0cdc10d2bd884c48f411b06a51f600.

--- src/usr/local/emhttp/plugins/npm-auto/webGui/settings.php
+++ src/usr/local/emhttp/plugins/npm-auto/webGui/settings.php
@@ -163,28 +163,26 @@ function set_toggle($data) {
 }
 
 //--- Main logic ---#
-if (isset($_REQUEST['action'])) {
-    header('Content-Type: application/json');
+header('Content-Type: application/json');
 
-    $action = $_REQUEST['action'];
-    write_log("Request received with action: " . $action);
+$action = $_REQUEST['action'] ?? '';
+write_log("Request received with action: " . $action);
 
-    switch ($action) {
-        case 'getSettings':
-            get_settings();
-            break;
-        case 'saveSettings':
-            save_settings($_POST);
-            break;
-        case 'getState':
-            get_state();
-            break;
-        case 'setToggle':
-            set_toggle($_POST);
-            break;
-        default:
-            write_log("Unknown action: " . $action);
-            echo json_encode(['ok' => false, 'error' => 'Unknown action.']);
-    }
+switch ($action) {
+    case 'getSettings':
+        get_settings();
+        break;
+    case 'saveSettings':
+        save_settings($_POST);
+        break;
+    case 'getState':
+        get_state();
+        break;
+    case 'setToggle':
+        set_toggle($_POST);
+        break;
+    default:
+        write_log("Unknown action: " . $action);
+        echo json_encode(['ok' => false, 'error' => 'Unknown action.']);
 }
 

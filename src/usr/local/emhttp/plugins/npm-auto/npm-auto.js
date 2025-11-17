c84162eefc8344150bd316f8eeafbc044db11738
dtrolley <77252655+dtrolley@users.noreply.github.com>
2025-11-17 15:12:58 -0500

    Revert "fix: remove csrf_token from setToggle request"
    This reverts commit 4de20707acb145e3d2eaba76c040122996180320.

--- src/usr/local/emhttp/plugins/npm-auto/npm-auto.js
+++ src/usr/local/emhttp/plugins/npm-auto/npm-auto.js
@@ -102,7 +102,7 @@
 
     $.post({
       url: '/plugins/npm-auto/webGui/settings.php?action=setToggle',
-      data: { container, enabled }
+      data: { container, enabled, csrf_token: csrfToken }
     });
   });
 })();
